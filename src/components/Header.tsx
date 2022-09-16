d8f9590de059f8e3c000836460aa80cbf2647eb9
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2022-09-16 18:38:56 +0200

    responive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,20 @@ import {
   MagnifyingGlassIcon,
   ShoppingCartIcon,
   UserIcon,
+  Bars3Icon,
 } from "@heroicons/react/24/outline";
 import Styles from "./Header.module.css";
 
 const Header = () => {
   return (
     <header className={Styles.container}>
-      <h1 className={Styles.title}>S K E I D A R</h1>
+      <div className={Styles.burgerAndTitleDiv}>
+        <div className={Styles.burgerDiv}>
+          <Bars3Icon className={Styles.burger} />
+          <p className={Styles.burgerText}>Meny</p>
+        </div>
+        <h1 className={Styles.title}>S K E I D A R</h1>
+      </div>
       <form className={Styles.form} action=''>
         <input placeholder='Søk' className={Styles.input} type='text' />
         <MagnifyingGlassIcon className={Styles.serarchIcon} />
